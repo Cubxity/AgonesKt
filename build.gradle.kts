0dadb765d7fc5344c7ff104054bfa97a10956ae9
Cubxity <contact@cubxity.dev>
2021-03-18 12:28:12 +0100

    chore: include protoc artifact + added javax.annotation-api to classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-import com.google.protobuf.gradle.generateProtoTasks
-import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.*
 
 plugins {
     kotlin("jvm") version "1.4.31"
@@ -24,6 +21,7 @@ dependencies {
     api("io.grpc:grpc-kotlin-stub:1.0.0")
 
     runtimeOnly("io.grpc:grpc-netty:1.36.0")
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 }
 
 protobuf {
@@ -34,6 +32,9 @@ protobuf {
         id("grpckt") {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:1.0.0:jdk7@jar"
         }
+    }
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.15.6"
     }
     generateProtoTasks {
         all().forEach {
