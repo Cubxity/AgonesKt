8f0ac54ebf687d47cb837cd89421758c7a51445b
Cubxity <contact@cubxity.dev>
2022-02-28 16:18:21 +0100

    build: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,42 @@
 import com.google.protobuf.gradle.*
 
 plugins {
-    kotlin("jvm") version "1.4.32"
-    id("com.google.protobuf") version "0.8.15"
+    kotlin("jvm") version "1.6.10"
+    id("com.google.protobuf") version "0.8.18"
     maven
 }
 
 group = "dev.cubxity.libs"
-version = "0.0.1"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    platform(kotlin("bom"))
     api(kotlin("stdlib"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
 
-    api("com.google.protobuf:protobuf-java-util:3.15.8")
-    api("io.grpc:grpc-kotlin-stub:1.0.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 
-    runtimeOnly("io.grpc:grpc-netty:1.37.0")
+    api("com.google.protobuf:protobuf-java-util:3.19.4")
+    api("io.grpc:grpc-kotlin-stub:1.2.1")
+
+    runtimeOnly("io.grpc:grpc-netty:1.44.1")
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 }
 
 protobuf {
     plugins {
         id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.36.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.44.1"
         }
         id("grpckt") {
-            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.0.0:jdk7@jar"
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.1:jdk7@jar"
         }
     }
     protoc {
-        artifact = "com.google.protobuf:protoc:3.15.6"
+        artifact = "com.google.protobuf:protoc:3.19.4"
     }
     generateProtoTasks {
         all().forEach {
