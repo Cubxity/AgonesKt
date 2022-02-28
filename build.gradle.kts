7cf96bf0ab7e42e2d172d58da2b7d2aef36a9537
Cubxity <contact@cubxity.dev>
2022-02-28 16:55:34 +0100

    fix(build): dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,26 +7,31 @@ plugins {
 }
 
 group = "dev.cubxity.libs"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    platform(kotlin("bom"))
-    api(kotlin("stdlib"))
+    api(platform(kotlin("bom")))
+    api(platform("io.grpc:grpc-bom:1.44.1"))
 
+    api(kotlin("stdlib"))
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 
-    api("com.google.protobuf:protobuf-java-util:3.19.4")
+    api("io.grpc:grpc-protobuf")
     api("io.grpc:grpc-kotlin-stub:1.2.1")
+    api("com.google.protobuf:protobuf-java-util:3.19.4")
+    api("com.google.protobuf:protobuf-kotlin:3.19.4")
 
     runtimeOnly("io.grpc:grpc-netty:1.44.1")
-    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 }
 
 protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.4"
+    }
     plugins {
         id("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.44.1"
@@ -35,11 +40,11 @@ protobuf {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.1:jdk7@jar"
         }
     }
-    protoc {
-        artifact = "com.google.protobuf:protoc:3.19.4"
-    }
     generateProtoTasks {
         all().forEach {
+            it.builtins {
+                id("kotlin")
+            }
             it.plugins {
                 id("grpc")
                 id("grpckt")
